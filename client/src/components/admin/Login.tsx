2a619e46973b45af948e314dd192b08f35caefbe
unknown <will-f1@hotmail.com>
2023-03-17 14:42:24 -0300

    Routeo front y tailwind agregados

---
+++ client/src/components/admin/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Login = () => {
+
+
+    return (
+        <div className="flex flex-col text-center items-center justify-center text-amber-500">
+            <h2 className="text-black text-[40px]">Hola asdasd</h2>
+        </div>
+    )
+};
